cb636a48ad2c004d564b787eddee8c673ff1e4b9
tfhgzeo <Gustavoacm-6@gmail.com>
2020-02-13 21:46:33 -0300

    Primeiras telas

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var btnAjuda = document.querySelector("#ajuda");
+btnAjuda.addEventListener('click', function (){
+    window.location.href = "ajuda.html";
+})
+
+var btnCadastrar = document.querySelector("#btnCadastrar");
+btnCadastrar.addEventListener("click", function () {
+    window.location.href = "signup.html";
+})
---
+++ js/signup.js
@@ -0,0 +1,5 @@
+var voltar = document.querySelector("i");
+
+voltar.addEventListener('click', function () {
+    window.location.href = "index.html";
+})
