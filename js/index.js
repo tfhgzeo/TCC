15037c29752fcd51cd62f3b614ed03bb54f21637
tfhgzeo <Gustavoacm-6@gmail.com>
2020-02-15 17:51:12 -0300

    Atualização de front-end

--- js/index.js
+++ js/index.js
@@ -7,3 +7,8 @@ var btnCadastrar = document.querySelector("#btnCadastrar");
 btnCadastrar.addEventListener("click", function () {
     window.location.href = "signup.html";
 })
+
+var btnEntrar = document.querySelector("#btnEntrar");
+btnEntrar.addEventListener("click", function () {
+    window.location.href = "login.html";
+})
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,42 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {        
+        onLoadFunction();
+    }
+};
+
+app.initialize();
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var estilo = document.getElementsByClassName('load');
+    estilo[0].style.visibility = "hidden";
+ });
+
 var voltar = document.querySelector("i");
 
 voltar.addEventListener('click', function () {
     window.location.href = "index.html";
 })
+
+
+var cadastrar = document.querySelector("#btnCadastrar");
+
+cadastrar.addEventListener("click", function (){
+    var nome = document.querySelector("#nome");
+    var email = document.querySelector("#email");
+    var senha = document.querySelector("#senha");
+    var senhaC = document.querySelector("#senhaC");
+
+    var emailValue = email.value;
+
+    if(emailValue.length == 0) {
+        document.querySelector(".email").classList.add("elementoIncorreto");
+    }else {
+        document.querySelector(".email").classList.remove("elementoIncorreto");
+    }
+})
+
